6632addc96e2da2009e32249d281ffe74ff5f0f2
ieuan0928 <ieuan0928@gmail.com>
2017-02-17 22:08:47 +0800

    commit

--- BOMBS.Client/BOMBS.UI.Foundation/Controls/StepContentNodes.xaml.cs
+++ BOMBS.Client/BOMBS.UI.Foundation/Controls/StepContentNodes.xaml.cs
@@ -8,22 +8,20 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BOMBS.UI.Foundation.Controls
 {
-    /// <summary>
-    /// Interaction logic for StepContentNodes.xaml
-    /// </summary>
     public partial class StepContentNodes : UserControl
     {
         public enum StateEnum : short
         {
-            Default = 0,
+            Active = 0,
             Disabled = -1,
-            Active = 1
+            Focused = 1
         }
 
         //public static readonly DependencyProperty StateProperty = DependencyProperty.Register("State", typeof(StateEnum), typeof(StepContentNodes), new PropertyMetadata(StateEnum.Default));
@@ -34,7 +32,7 @@ namespace BOMBS.UI.Foundation.Controls
         }
 
 
-        private StateEnum _state = StateEnum.Default;
+        private StateEnum _state = StateEnum.Active;
         public StateEnum State
         {
             get { return _state; }
@@ -43,11 +41,45 @@ namespace BOMBS.UI.Foundation.Controls
 
         public override void OnApplyTemplate()
         {
-            if (State == StateEnum.Active)
+            switch (State)
             {
-                lbl.Background = new SolidColorBrush(Colors.White );
+                case StateEnum.Active:
+                    nodeTextBlock.Cursor = Cursors.Hand;
+
+                    nodeTextBlock.MouseEnter += NodeTextBlock_MouseEnter;
+                    nodeTextBlock.MouseLeave += NodeTextBlock_MouseLeave;
+                    break;
+
+                case StateEnum.Focused:
+                    nodeLabel.Background = new SolidColorBrush(Colors.White);
+                    break;
+                case StateEnum.Disabled:
+                    nodeTextBlock.Foreground = new SolidColorBrush(Color.FromRgb(180, 180, 180));
+                    nodeTextBlock.Effect = new DropShadowEffect()
+                    {
+                        Direction = 240,
+                        BlurRadius = 0,
+                        ShadowDepth = 1.5,
+                        RenderingBias = RenderingBias.Quality,
+                        Color = Color.FromArgb(125, 255, 255, 255)
+                    };
+                    break;
+
             }
+
             base.OnApplyTemplate();
+        }
+
+        private void NodeTextBlock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            TextBlock me = (TextBlock)sender;
+            me.TextDecorations = null;
+        }
+
+        private void NodeTextBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock me = (TextBlock)sender;
+            me.TextDecorations = TextDecorations.Underline;           
         }
     }
 }
